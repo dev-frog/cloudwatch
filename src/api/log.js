00beef1dc3eb2ff0f464eaf1a60e1f99011f68aa
dev-frog <kali.nisga@gmail.com>
2022-04-08 16:13:01 +0600

    Geting Logs from Cloudwatch

--- src/api/log.js
+++ src/api/log.js
@@ -8,30 +8,42 @@ AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
-const cwl = new AWS.CloudWatch({ apiVersion: process.env.AWS_API_VERSION });
+const cwl = new AWS.CloudWatchLogs({ apiVersion: process.env.AWS_API_VERSION });
 
 router.get('/', (req, res) => {
   // Get Aws CloudWatch Logs using Credentials
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatchLogs.html#describeLogGroups-property
 
   var params = {
-    // logGroupName: '/aws/appsync/apis/xjhoxpb2sjdhfgs7cvlsinc2ge',
-    // logStreamNamePrefix: '2022/04/',
+    logGroupName: '/aws/appsync/apis/xjhoxpb2sjdhfgs7cvlsinc2ge',
+    descending: true,
     // limit: 10,
-    Dimensions: [
-      {
-        Name: '/aws/appsync/apis/xjhoxpb2sjdhfgs7cvlsinc2ge' /* required */,
-      },
-    ],
-    MetricName: 'IncomingLogEvents',
-    Namespace: 'AWS/Logs',
+    // Dimensions: [
+    //   {
+    //     Name: '/aws/appsync/apis/xjhoxpb2sjdhfgs7cvlsinc2ge' /* required */,
+    //   },
+    // ],
+    // MetricName: 'IncomingLogEvents',
+    // Namespace: 'AWS/Logs',
   };
 
-  cwl.listMetrics(params, function (err, data) {
+  cwl.describeLogStreams(params, function (err, data) {
     if (err) {
       console.log(err, err.stack);
     } else {
       console.log(data);
+      // conver time to readable format
+      data.logStreams.forEach((stream) => {
+        stream.lastEventTimestamp = new Date(
+          stream.lastEventTimestamp
+        ).toLocaleString();
+        stream.creationTime = new Date(stream.creationTime).toLocaleString();
+        stream.firstEventTimestamp = new Date(
+          stream.firstEventTimestamp
+        ).toLocaleString();
+      });
+      // Get Details of Log Streams
+      // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatchLogs.html#describeLogStreams-property
       res.send(data);
     }
   });
