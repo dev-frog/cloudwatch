48793231b09db9a3f22eb4387221c92ecd8bcb58
dev-frog <kali.nisga@gmail.com>
2022-04-11 12:50:44 +0600

    Now Get 24 hours logs

--- src/api/log.js
+++ src/api/log.js
@@ -10,13 +10,13 @@ AWS.config.update({
 });
 const cwl = new AWS.CloudWatchLogs({ apiVersion: process.env.AWS_API_VERSION });
 
-router.get('/', (req, res) => {
+async function getLogStreams() {
   // Get Aws CloudWatch Logs using Credentials
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatchLogs.html#describeLogGroups-property
-
   var params = {
     logGroupName: '/aws/appsync/apis/xjhoxpb2sjdhfgs7cvlsinc2ge',
     descending: true,
+    orderBy: 'LastEventTime',
     // limit: 10,
     // Dimensions: [
     //   {
@@ -44,7 +44,39 @@ router.get('/', (req, res) => {
       });
       // Get Details of Log Streams
       // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatchLogs.html#describeLogStreams-property
-      res.send(data);
+      return data;
+    }
+  });
+}
+
+// function generate 13 digit unix timestamp  start and end time
+function generateTimeStamp() {
+  var date = new Date();
+  var timestamp = date.getTime();
+  var startTime = timestamp - 1000 * 60 * 60 * 24 * 7;
+  var endTime = timestamp;
+  return { startTime, endTime };
+}
+
+router.get('/', (req, res) => {
+  console.log(generateTimeStamp());
+  const { startTime, endTime } = generateTimeStamp();
+  const response = {
+    logGroupName: '/aws/appsync/apis/xjhoxpb2sjdhfgs7cvlsinc2ge',
+    startTime: startTime,
+    endTime: endTime,
+  };
+  cwl.filterLogEvents(response, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      // console.log(data);
+      data.events.forEach((stream) => {
+        // console.log(data.events.length);
+        stream.timestamp = new Date(stream.timestamp).toLocaleString();
+        stream.ingestionTime = new Date(stream.ingestionTime).toLocaleString();
+      });
+      res.json(data);
     }
   });
 });
